25eeff9a8d6024823ea8118221c7536665660633
JoaoKrapp <joaokrappcurso@gmail.com>
2021-12-01 01:56:58 -0300

    Remaster 1.0 Co-authored-by: Had781795 <Had781795@users.noreply.github.com> Co-authored-by: Nicolas Z <nicolasfvz@users.noreply.github.com>

--- src/profiles/tests.py
+++ src/profiles/tests.py
@@ -1,7 +1,5 @@
-from django.test import TestCase
-import markdown
-
+#from django.test import TestCase
+#import markdown
+# 7, 11
 # Create your tests here.
-
-html = markdown.markdown("**![image](https://i.pinimg.com/280x280_RS/cb/b2/80/cbb280fa8c687cf3b137df878bf82d08.jpg)**")
-print(html)
+ 

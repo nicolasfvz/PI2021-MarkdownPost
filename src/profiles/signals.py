76d018f1a8f7372bf6fc780d789a8874d63674b5
JoaoKrapp <joaokrappcurso@gmail.com>
2021-12-14 01:45:48 -0300

    monkes generator

--- src/profiles/signals.py
+++ src/profiles/signals.py
@@ -4,8 +4,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 import random
 
-def picture():
-    return f'monkes/monke{random.randint(0, 30)}.png'
+from .macacos.NFTGenerator import picture
+
+#def picture():
+#    return f'monkes/monke{random.randint(0, 30)}.png'
     
 @receiver(post_save, sender=User)
 def post_save_create_profile(sender, instance, created, **kwargs):
